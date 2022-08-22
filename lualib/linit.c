471b683c0a40fb25d4ab2aedd972442c38c34631
YI <uuuuuu@protonmail.com>
2022-08-22 16:50:21 +0800

    add more functions from lua std lib

--- lualib/linit.c
+++ lualib/linit.c
@@ -35,15 +35,15 @@
 ** program
 */
 static const luaL_Reg loadedlibs[] = {{LUA_GNAME, luaopen_base},
-                                      // {LUA_LOADLIBNAME, luaopen_package},
-                                      // {LUA_COLIBNAME, luaopen_coroutine},
-                                      // {LUA_TABLIBNAME, luaopen_table},
-                                      // {LUA_IOLIBNAME, luaopen_io},
+                                      {LUA_LOADLIBNAME, luaopen_package},
+                                      {LUA_COLIBNAME, luaopen_coroutine},
+                                      {LUA_TABLIBNAME, luaopen_table},
+                                      {LUA_IOLIBNAME, luaopen_io},
                                       // {LUA_OSLIBNAME, luaopen_os},
-                                      // {LUA_STRLIBNAME, luaopen_string},
+                                      {LUA_STRLIBNAME, luaopen_string},
                                       // {LUA_MATHLIBNAME, luaopen_math},
-                                      // {LUA_UTF8LIBNAME, luaopen_utf8},
-                                      // {LUA_DBLIBNAME, luaopen_debug},
+                                      {LUA_UTF8LIBNAME, luaopen_utf8},
+                                      {LUA_DBLIBNAME, luaopen_debug},
                                       {NULL, NULL}};
 
 LUALIB_API void luaL_openlibs(lua_State *L) {
