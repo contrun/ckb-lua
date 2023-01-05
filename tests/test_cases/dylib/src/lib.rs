2b98800e8ec6aba814a4b765133313172ba0d31d
YI <uuuuuu@protonmail.com>
2023-01-05 11:29:42 +0800

    copy code from secp256r1 tests

--- tests/test_cases/dylib/src/lib.rs
+++ tests/test_cases/dylib/src/lib.rs
@@ -1,14 +1,12 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+//! pub use const BUNDLED_CELL: Files
+//! pub use const CODE_HASH_DAO: [u8; 32]
+//! pub use const CODE_HASH_SECP256K1_BLAKE160_SIGHASH_ALL: [u8; 32]
+//! pub use const CODE_HASH_SECP256K1_RIPEMD160_SHA256_SIGHASH_ALL: [u8; 32]
+
+#![allow(clippy::unreadable_literal)]
+
+include!(concat!(env!("OUT_DIR"), "/bundled.rs"));
+include!(concat!(env!("OUT_DIR"), "/code_hashes.rs"));
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod tests;
